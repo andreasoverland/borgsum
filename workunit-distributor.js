8af855aa3eeab5a4b9ab281d1265b1b2d25dc152
Andreas Øverland <andreasoverland@gmail.com>
2018-11-30 17:23:40 +0100

    workunit dispatcher works

--- workunit-distributor.js
+++ workunit-distributor.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 const exec = require('child_process').exec;
 
 let workunits = [];
+let numberOfWorkunits = 0;
+
+let startTime = new Date().getTime();
 
 fs.readFile("settings.txt", (err,res) => {
 
@@ -22,13 +25,13 @@ fs.readFile("settings.txt", (err,res) => {
 
   fs.readFile( settings.workunits , (werr,wres) => {
 
-      workunits = Buffer.from(wres).toString("UTF-8").split("\n");
+      workunits = Buffer.from(wres).toString("UTF-8").trim().split("\n");
 
       workunits.shuffle();
-
-      console.log( workunits );
+      numberOfWorkunits = workunits.length;
 
       for( let numthreads = 0; numthreads < settings.numthreads ; numthreads++ ){
+
           calculateNextUnit( settings );
       }
 
@@ -37,14 +40,64 @@ fs.readFile("settings.txt", (err,res) => {
 
 });
 
+let counts = {};// level {} -> Metric:N
+let statsReported = false;
+let numFinished = 0;
 
 function calculateNextUnit( settings ){
     let workunit = workunits.shift();
     console.log( new Date() + " Starting workunit " + workunit + " Remaining units " + workunits.length );
     os.execCommand("./chessengine " + workunit + " " + settings.depth, result => {
-        // console.log( result );
-        if( workunits.length > 0 ){
+
+        numFinished++;
+
+        result = result.substring( result.indexOf("Depth"));
+        result = result.substring( 0, result.indexOf("Total"));
+
+        result = result.replace(/ /g, '');
+
+
+        // TODO: accumulate into counts
+        lines = result.split("\n");
+        lines.forEach( ll => {
+           if( ll.indexOf("Depth") < 0 && ll.length > 2) {
+                let l = ll.split("\t");
+                let d = l[0];
+                if( counts[d] ){
+                    let c = counts[d];
+                    c.nodes += parseInt(l[1]);
+                    c.caps += parseInt(l[2]);
+                    c.ep += parseInt(l[3]);
+                    c.castles += parseInt(l[4]);
+                    c.promos += parseInt(l[5]);
+                    c.checks += parseInt(l[6]);
+                    c.mates += parseInt(l[7]);
+                }
+                else {
+                    let c = {};
+                    c.nodes = parseInt(l[1]);
+                    c.caps = parseInt(l[2]);
+                    c.ep = parseInt(l[3]);
+                    c.castles = parseInt(l[4]);
+                    c.promos = parseInt(l[5]);
+                    c.checks = parseInt(l[6]);
+                    c.mates = parseInt(l[7]);
+                    counts[ d ] = c;
+                }
+
+           }
+
+
+        });
+
+
+        if( workunits.length > 0  ){
             calculateNextUnit(settings);
+        }
+        if(  numFinished === numberOfWorkunits ){
+            console.log(counts);
+            let endTime = new Date().getTime();
+            console.log( "millis:", endTime-startTime);
         }
     });
 }
