69250bfe5a3a92b0126ed7059f6e679af584e018
Andreas Øverland <andreasoverland@gmail.com>
2018-12-16 11:40:32 +0100

    updated url for client

--- client.sh
+++ client.sh
@@ -5,5 +5,5 @@ echo "Starting Borgsum Chessengine Workunit Client";
 
 while true
 do
-    curl -s http://localhost:4030/postresult/ -d "result=`curl -s localhost:4030 |xargs ./chessengine`"
+    curl -s https://www.andreasoverland.no/chessengine/workunits/postresult/ -d "result=`curl -s https://www.andreasoverland.no/chessengine/workunits/ |xargs ./chessengine`"
 done
