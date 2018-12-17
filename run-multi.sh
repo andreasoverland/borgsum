e44b948f9b74bd74362cce6dfae5d6c60e95a825
Andreas Øverland <andreasoverland@gmail.com>
2018-12-17 14:15:36 +0100

    client with timing, activates engined outside office hours

--- run-multi.sh
+++ run-multi.sh
@@ -1,31 +1,31 @@
 {
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ P....... .PPPPPPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-00.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ P....... ........ .PPPPPPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-01.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ .P...... P.PPPPPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-02.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ .P...... ........ P.PPPPPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-03.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ ..P..... PP.PPPPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-04.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ P....... .PPPPPPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-00.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ..P..... ........ PP.PPPPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-05.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ .P...... P.PPPPPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-02.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ .P...... ........ P.PPPPPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-03.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ ..P..... PP.PPPPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-04.txt ;
 } &
 
 {
-./chessengine "rnbqkbnr pppppppp ........ ........ ..P..... ........ PP.PPPPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-05.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ ...P.... PPP.PPPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-06.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ...P.... ........ PPP.PPPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-07.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ ....P... PPPP.PPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-08.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ....P... ........ PPPP.PPP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-09.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ ....P... PPPP.PPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-08.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ P....... ........ .PPPPPPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-01.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ ......P. PPPPPP.P RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-12.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ ...P.... PPP.PPPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-06.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ N....... PPPPPPPP R.BQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-16.txt ;
 } &
 
 {
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ .....P.. PPPPP.PP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-10.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ .....P.. ........ PPPPP.PP RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-11.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ ......P. PPPPPP.P RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-12.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ......P. ........ PPPPPP.P RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-13.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ .......P PPPPPPP. RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-14.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ....P... ........ PPPP.PPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-09.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ .....P.. ........ PPPPP.PP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-11.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ......P. ........ PPPPPP.P RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-13.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ .......P PPPPPPP. RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-14.txt ;
 } &
 
 {
-./chessengine "rnbqkbnr pppppppp ........ ........ .......P ........ PPPPPPP. RNBQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-15.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ N....... PPPPPPPP R.BQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-16.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ ..N..... PPPPPPPP R.BQKBNR" 8 b > ~/Dropbox/ChessEngine/multitest-8-17.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ .....N.. PPPPPPPP RNBQKB.R" 8 b > ~/Dropbox/ChessEngine/multitest-8-18.txt ;
-./chessengine "rnbqkbnr pppppppp ........ ........ ........ .......N PPPPPPPP RNBQKB.R" 8 b > ~/Dropbox/ChessEngine/multitest-8-19.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ .....P.. PPPPP.PP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-10.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ .......P ........ PPPPPPP. RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-15.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ...P.... ........ PPP.PPPP RNBQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-07.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ ..N..... PPPPPPPP R.BQKBNR" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-17.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ .....N.. PPPPPPPP RNBQKB.R" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-18.txt ;
+./chessengine "rnbqkbnr pppppppp ........ ........ ........ .......N PPPPPPPP RNBQKB.R" b 15 8 > ~/Dropbox/ChessEngine/multitest-8-19.txt ;
 } &
