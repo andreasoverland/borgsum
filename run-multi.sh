012c896f70f23e4297c846be51bd4a77f25cb311
Andreas Øverland <andreasoverland@gmail.com>
2019-08-05 16:40:57 +0200

    multirun update

--- run-multi.sh
+++ run-multi.sh
@@ -1,31 +1,31 @@
-#{
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ P....... .PPPPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >  /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-a.txt ;
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ..P..... ........ PP.PPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >> /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-a.txt ;
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .P...... P.PPPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >> /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-a.txt ;
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ .P...... ........ P.PPPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >> /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-a.txt ;
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ..P..... PP.PPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >> /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-a.txt ;
-#} &
-
-{
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ....P... PPPP.PPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-b0.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ P....... ........ .PPPPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-b1.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ......P. PPPPPP.P RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-b2.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ...P.... PPP.PPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-b3.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ N....... PPPPPPPP R.BQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-b4.txt ;
+{./chessengine -maxlevel 7 -logtype binary|grep "%"|cut -b2- >
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ P....... .PPPPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-a0.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ..P..... ........ PP.PPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-a1.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .P...... P.PPPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-a2.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ .P...... ........ P.PPPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-a3.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ..P..... PP.PPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-a4.txt ;
 } &
 
-#{
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ....P... ........ PPPP.PPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >  /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-c.txt ;
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ .....P.. ........ PPPPP.PP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >> /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-c.txt ;
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ......P. ........ PPPPPP.P RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >> /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-c.txt ;
-#./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .......P PPPPPPP. RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" >> /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-c.txt ;
-#} &
+{
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ....P... PPPP.PPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-b0.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ P....... ........ .PPPPPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-b1.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ......P. PPPPPP.P RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-b2.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ...P.... PPP.PPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-b3.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ N....... PPPPPPPP R.BQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-b4.txt ;
+} &
 
 {
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .....P.. PPPPP.PP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-d1.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ .......P ........ PPPPPPP. RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-d2.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ...P.... ........ PPP.PPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-d3.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ..N..... PPPPPPPP R.BQKBNR b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-d4.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .....N.. PPPPPPPP RNBQKB.R b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-d5.txt ;
-./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .......N PPPPPPPP RNBQKB.R b KQkq - 1" -maxlevel 7 -logtype diagram |grep "%" > /Volumes/Photos2019/ChessEngine/results/default/level-7-all-part-d6.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ....P... ........ PPPP.PPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-c0.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ .....P.. ........ PPPPP.PP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-c1.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ......P. ........ PPPPPP.P RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-c2.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .......P PPPPPPP. RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-c3.txt ;
+} &
+
+{
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .....P.. PPPPP.PP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-d1.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ .......P ........ PPPPPPP. RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-d2.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ...P.... ........ PPP.PPPP RNBQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-d3.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ ..N..... PPPPPPPP R.BQKBNR b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-d4.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .....N.. PPPPPPPP RNBQKB.R b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-d5.txt ;
+./chessengine -diagram "rnbqkbnr pppppppp ........ ........ ........ .......N PPPPPPPP RNBQKB.R b KQkq - 1" -maxlevel 7 -logtype binary |grep "%"|cut -b2- > ~/Desktop/level-7-all-part-d6.txt ;
 } &
