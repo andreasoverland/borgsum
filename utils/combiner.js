e96febb761a01092d071e8061a3a2364ec4fe447
Andreas Øverland <andreasoverland@gmail.com>
2019-09-10 23:18:12 +0200

    combiner doing work

--- utils/combiner.js
+++ utils/combiner.js
@@ -1,7 +1,7 @@
 #!/usr/local/bin/node
 
 const fs = require('fs');
-const readline = require('readline');
+
 // åpne første fil i folderen (antar at det er mange filer som skal kombineres, multipleres og reduseres)
 // les 1000 første unike linjer/brett
 //  alt må skje sync
@@ -11,87 +11,134 @@ const readline = require('readline');
 //  skriv alle kombinerte linjer i en ny fil
 // les nye 1000 (ubrukte) linjer, og repeter
 
-
-
-let masterLine = "";
-
-let masterLineFileName = "xaa";
-
-let lineReader = readline.createInterface({
-	input: fs.createReadStream(masterLineFileName)
-});
-
-let bytesRead = 0;
-
-lineReader.on('line', function (line) {
-	bytesRead = masterLine.length+1;
-	if( !line.startsWith("#") ) {
-		masterLine = line;
-		lineReader.pause();
-		console.log( "Master line read : "+ masterLine +"\nPreparing for big scan");
-		prepareScan();
-	}
-});
-
-/*lineReader.on('close', ()=>{
-	if( masterLines.length < numMasterLines ){
-		prepareScan();
-	}
-});*/
-
-
-let writePosition = 0;
-
-function prepareScan(){
-
-
-	let p = masterLine.split("m");
-
-	if (map[p[0]] === undefined) {
-		map[p[0]] = parseInt(p[1]);
-	} else {
-		map[p[0]] += parseInt(p[1]);
-	}
-	masterLine = "#" + masterLine.substring(1);
-
-	console.log( "Ready for big scan with initial map:\n",map);
-
-	let file = fs.openSync(masterLineFileName,"rs+");
-	fs.writeSync(file, masterLine, writePosition, "UTF-8");
-	writePosition += bytesRead;
-	bytesRead = 0;
-
-	fs.closeSync(file);
-
-	scanAllFilesForMasterLines( ); // bigtime
-
-	fs.writeFileSync("../combined.txt", Buffer.from( JSON.stringify(map), "utf-8"), {flag: 'a'});
-	map = {};
-	// lineReader.resume();
-}
+let numMasterLines = 10000;
+let masterLines = [];
+let masterLineFiles = fs.readdirSync(".");
 
 let numLinesReadAndChecked = 0;
 let map = {};
 let scanLineFiles = fs.readdirSync(".");
 
-function scanAllFilesForMasterLines(){
+let masterLineFileName = masterLineFiles.shift();
+let masterLineFilePosition = 0;
 
-	for( let i=0;i<scanLineFiles.length;i++){
-		let file = fs.openSync(scanLineFiles[i],"rs+");
-		scanAndMarkLinesInFile(file);
-		fs.closeSync(file);
+let moreMasterLinesAvailable = true;
+
+fs.writeFileSync("../combined.txt", Buffer.from( "", "utf-8") );
+
+while( moreMasterLinesAvailable ){
+	masterLines = [];
+	let currentFileName = masterLineFileName;
+	readNextMasterLinesFromNextFile();
+	console.log( "Number of lines read from " + currentFileName + " ", Object.keys(map).length);
+	prepareScan(currentFileName);
+}
+// les ut og marker fra en fil, og så er man "ferdig" med den filen.
+// når alle filer er gjennomgått tar vi en ny runde på samme fila, helt til det kun er #-linjer i den fila,
+// hvorpå vi begynner å lese fra neste fil.
+
+
+function readNextMasterLinesFromNextFile(){
+	let size = 100000;
+	let file = fs.openSync(masterLineFileName,"rs+");
+
+	let readBuffer = Buffer.alloc(size);
+	let numRead = fs.readSync(file,readBuffer,0,size,masterLineFilePosition);
+
+	while( Object.keys(map).length < numMasterLines ){
+
+		let lines = readBuffer.toString("UTF-8").trim().split("\n");
+
+		if (numRead === size) {
+			let lastLine = lines.pop();
+			let lengthOfLastLine = lastLine.length;
+			numRead -= lengthOfLastLine; // for next read
+		}
+
+		let dirty = false;
+
+		for (let i = 0; i < lines.length; i++) {
+			if ( !lines[i].startsWith("#") && lines[i].length < 100 ) {
+
+				let p = lines[i].split("m");
+				if (map[p[0]] === undefined ) {
+					map[p[0]] = parseInt(p[1]);
+					lines[i] = "#" + lines[i].substring(1);
+					dirty = true;
+				}
+				else {
+					map[p[0]] += parseInt(p[1]);
+					lines[i] = "#" + lines[i].substring(1);
+				}
+			}
+		}
+
+		if (dirty) {
+			let writeBuffer = lines.join("\n");
+			fs.writeSync(file, writeBuffer, masterLineFilePosition, "UTF-8");
+		}
+
+		masterLineFilePosition += numRead;
+
+		readBuffer = Buffer.alloc(size);
+		numRead = fs.readSync(file, readBuffer, 0, size, masterLineFilePosition);
+
+		if( numRead <= 0 && masterLineFiles.length > 0){
+			masterLineFileName = masterLineFiles.shift();
+			masterLineFilePosition = 0;
+			console.log("** Next master line file :", masterLineFileName );
+			scanLineFiles.shift();
+			break;
+		}
+		else if ( numRead <= 0 && masterLineFiles.length === 0 ){
+			console.log("***** No more master lines available.");
+			moreMasterLinesAvailable = false;
+			break;
+		}
 	}
-	console.log( "Partial after round one.\n", map );
+
+	fs.closeSync(file);
+
 }
 
 
+function prepareScan(currentFileName){
+
+	console.log( "Ready for big scan with " + Object.keys(map).length + " master lines");
+	if( Object.keys(map).length === 4330 ){
+		fs.writeFileSync("buggy-keys.json", Buffer.from( JSON.stringify(map,null,2), "utf-8"));
+	}
+	scanAllFilesForMasterLines(currentFileName); // bigtime
+
+	console.log( "Scanned. Writing map.");
+	let buff = "";
+	Object.keys(map).forEach( k=>{
+		buff += (k + "m" + map[k] + "\n");
+	});
+	fs.writeFileSync("../combined.txt", Buffer.from( buff, "utf-8"), {flag: 'a'});
+	map = {};
+
+}
+
+function scanAllFilesForMasterLines(currentFileName){
+
+	for( let i=0;i<scanLineFiles.length;i++){
+		let file = fs.openSync(scanLineFiles[i],"rs+");
+		process.stdout.write( scanLineFiles[i] + " " );
+		let readStartPos = 0;
+		if( currentFileName === scanLineFiles[i]){
+			readStartPos = masterLineFilePosition;
+		}
+		scanAndMarkLinesInFile(file,readStartPos);
+		fs.closeSync(file);
+	}
+	process.stdout.write("\n");
+}
 
 
-function scanAndMarkLinesInFile( file ) {
+function scanAndMarkLinesInFile( file ,readStartPos) {
  	let size = 100000;
-
-	let position = 0;
-
+	let position = readStartPos;
 	let buffer = Buffer.alloc(size);
 	let numRead = fs.readSync(file, buffer, 0, size, position);
 
@@ -100,8 +147,8 @@ function scanAndMarkLinesInFile( file ) {
 		let lines = buffer.toString("utf-8").trim().split("\n");
 
 		if (numRead === size) {
-			lastLine = lines.pop();
-			lengthOfLastLine = lastLine.length;
+			let lastLine = lines.pop();
+			let lengthOfLastLine = lastLine.length;
 			numRead -= lengthOfLastLine; // for next read
 		}
 
@@ -110,30 +157,26 @@ function scanAndMarkLinesInFile( file ) {
 		let dirty = false;
 
 		for (let i = 0; i < lines.length; i++) {
-			if (lines[i].startsWith("#")) {
-				break;
-			}
-			let p = lines[i].split("m");
-			if (map[p[0]] !== undefined) {
-				map[p[0]] += parseInt(p[1]);
-				lines[i] = "#" + lines[i].substring(1);
-				dirty = true;
+			if ( !lines[i].startsWith("#") && lines[i].length < 100 ) {
+				let p = lines[i].split("m");
+				if (map[p[0]] !== undefined) {
+					map[p[0]] += parseInt(p[1]);
+					lines[i] = "#" + lines[i].substring(1);
+					dirty = true;
+				}
 			}
 		}
-
 
 		if (dirty) {
 			let writeBuffer = lines.join("\n");
 			fs.writeSync(file, writeBuffer, position, "UTF-8");
 		}
 
-
 		position += numRead;
 
 		buffer = Buffer.alloc(size);
 		numRead = fs.readSync(file, buffer, 0, size, position);
 	}
-
 
 }
 
