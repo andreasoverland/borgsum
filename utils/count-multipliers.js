47877ba4200a5501eb36854760eac796bfb57e53
Andreas Øverland <andreasoverland@gmail.com>
2019-09-12 21:32:20 +0200

    count-multipliers

--- utils/count-multipliers.js
+++ utils/count-multipliers.js
@@ -1,20 +1,12 @@
 #!/usr/local/bin/node
 
-const fs = require('fs');
-
 
 var lineReader = require('readline').createInterface({
 	input: require('fs').createReadStream( "combined.txt" )
 });
 
-fs.writeFileSync("combined.txt", Buffer.from("", "UTF-8"));
-
 let numDifferentBoards = 0;
 let numBoards = 0;
-let lastLine = "";
-let lastMultiplier = 0;
-let buffer = "";
-let duplicates = 0;
 
 lineReader.on('line', function (line) {
 	if( line.trim() === "" ){
@@ -23,32 +15,14 @@ lineReader.on('line', function (line) {
 
 	let l = line.trim().split("m");
 	let multiplier = parseInt(l[1]);
-	if( l[0] === lastLine ){
-		lastMultiplier += multiplier;
-		duplicates++;
-	}
-	else {
-		if( lastLine !== "" ) {
-			buffer += lastLine + "m" + lastMultiplier + "\n";
-			numDifferentBoards++;
-			numBoards+= lastMultiplier;
-			
-		}
-		lastLine = l[0];
-		lastMultiplier = multiplier;
-	}
+	numDifferentBoards++;
+	numBoards+= multiplier;
 
 });
 
 lineReader.on('close', ()=>{
-	if( buffer.length > 0 ){
-		buffer += lastLine + "m" + lastMultiplier + "\n";
-		numDifferentBoards++;
-		numBoards+= lastMultiplier;
-		fs.writeFileSync("combined.txt", Buffer.from(buffer, "utf-8"), {flag: 'a'});
-	}
+
 	console.log( "Num unique boards :",numDifferentBoards);
 	console.log( "Num boards        :",numBoards);
-	console.log( "Duplicates found  :",duplicates);
 
 });
